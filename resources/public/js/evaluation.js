9cf390a52da6993f4089369eda0ec793c876235e
Jindrich Mynarz <mynarzjindrich@gmail.com>
2016-07-19 15:14:31 +0200

    JavaScript to reveal the exercise's solution

--- resources/public/js/evaluation.js
+++ resources/public/js/evaluation.js
@@ -2,26 +2,30 @@
   var yasqeOptions = {
     createShareLink: false,
     readOnly: true,
-    persistent: null
+    persistent: null,
+    theme: "sparqlab"
   };
-  var textAreaToYasqe = function (id) {
-    var textArea = document.getElementById(id);
-    
-    if (textArea) {
-      var query = textArea.value,
-          yasqe = YASQE.fromTextArea(textArea, yasqeOptions);
+  var textAreaToYasqe = function (element) {
+    if (element) {
+      var query = element.value,
+          yasqe = YASQE.fromTextArea(element, yasqeOptions);
       yasqe.setValue(query);
       return yasqe;
     }
   };
 
   $(document).ready(function () {
-    var query = textAreaToYasqe("query"),
-        canonicalQuery = textAreaToYasqe("canonical-query"),
-        queryResults = YASR(document.getElementById("query-results")),
+    var query = textAreaToYasqe(document.getElementById("query")),
+        $canonicalQuery = $("#canonical-query"),
+        queryResults = YASR(document.getElementById("query-results"), yasrConfig),
         $results = $("#results"),
-        canonicalQueryResults = YASR(document.getElementById("canonical-query-results")),
-        $canonicalResults = $("#canonical-results");
+        canonicalQueryResults = YASR(document.getElementById("canonical-query-results"), yasrConfig),
+        $canonicalResults = $("#canonical-results"),
+        $revealSolution = $("#reveal-solution");
+       
+    if (!$canonicalQuery.hasClass("invisible")) {
+      canonicalQuery = textAreaToYasqe($canonicalQuery[0]);
+    }
     queryResults.setResponse({
       response: $results.val(),
       contentType: $results.data("type")
@@ -29,6 +33,11 @@
     canonicalQueryResults.setResponse({
       response: $canonicalResults.val(),
       contentType: $canonicalResults.data("type")
+    });
+    $revealSolution.on("click", function (e) {
+      $canonicalQuery.removeClass("invisible");
+      canonicalQuery = textAreaToYasqe($canonicalQuery[0]);
+      $("#reveal-solution-button").hide();
     });
   });
 })(jQuery);
