9ebc040f79a04f887afeb09e642e8bfff224f7c6
Jindrich Mynarz <mynarzjindrich@gmail.com>
2016-07-19 21:15:43 +0200

    Namespace prefixes

---
+++ src/clj/sparqlab/prefixes.clj
@@ -0,0 +1,14 @@
+(ns sparqlab.prefixes)
+
+; ----- Namespace prefixes -----
+
+(defn- prefix
+  "Builds a function for compact IRIs in the namespace `iri`."
+  [iri]
+  (partial str iri))
+
+(def rdfs
+  (prefix "http://www.w3.org/2000/01/rdf-schema#"))
+
+(def xsd
+  (prefix "http://www.w3.org/2001/XMLSchema#"))
