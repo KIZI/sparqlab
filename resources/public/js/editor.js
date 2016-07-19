12461f17dfd8972ca041106c282e7641f21a0970
Jindrich Mynarz <mynarzjindrich@gmail.com>
2016-07-19 15:12:15 +0200

    Disable control buttons when SPARQL query is syntactically invalid

--- resources/public/js/editor.js
+++ resources/public/js/editor.js
@@ -1,6 +1,7 @@
 (function ($) {
   $(document).ready(function () {
     var $controlButtons = $(".control-buttons"),
+        $controlButtonsControls = $controlButtons.find("button"),
         $codeMirror = $(".CodeMirror"),
         $results = $("#results");
 
@@ -19,40 +20,10 @@
       theme: "sparqlab"
     });
 
-    YASR.plugins.table.defaults.fetchTitlesFromPreflabel = false;
-    YASR.plugins.table.defaults.datatable.language = {
-      "sEmptyTable":     "Tabulka neobsahuje žádná data",
-      "sInfo":           "Zobrazuji _START_ až _END_ z celkem _TOTAL_ záznamů",
-      "sInfoEmpty":      "Zobrazuji 0 až 0 z 0 záznamů",
-      "sInfoFiltered":   "(filtrováno z celkem _MAX_ záznamů)",
-      "sInfoPostFix":    "",
-      "sInfoThousands":  " ",
-      "sLengthMenu":     "Zobraz záznamů _MENU_",
-      "sLoadingRecords": "Načítám...",
-      "sProcessing":     "Provádím...",
-      "sSearch":         "Hledat:",
-      "sZeroRecords":    "Žádné záznamy nebyly nalezeny",
-      "oPaginate": {
-        "sFirst":    "První",
-        "sLast":     "Poslední",
-        "sNext":     "Další",
-        "sPrevious": "Předchozí"
-      },
-      "oAria": {
-        "sSortAscending":  ": aktivujte pro řazení sloupce vzestupně",
-        "sSortDescending": ": aktivujte pro řazení sloupce sestupně"
-      }
-    };
-
-    var yasr = YASR($results[0], {
-      getUsedPrefixes: yasqe.getPrefixesFromQuery,
-      outputPlugins: ["error", "boolean", "rawResponse", "table"],
-      persistency: {
-        prefix: null
-      },
-      useGoogleCharts: false
-    });
-    yasqe.setValue("\nSELECT * \nWHERE {\n  ?s ?p ?o .\n}\nLIMIT 10");
+    var yasr = YASR($results[0], jQuery.extend(yasrConfig, {
+      getUsedPrefixes: yasqe.getPrefixesFromQuery
+    }));
+    //yasqe.setValue("\nSELECT * \nWHERE {\n  ?s ?p ?o .\n}\nLIMIT 10");
     yasqe.addPrefixes(prefixes);
 
     // Event handlers
@@ -70,5 +41,12 @@
         yasr.setResponse(xhr, textStatus);
       },
     };
+    yasqe.on("change", function (e) {
+      if (yasqe.queryValid) {
+        $controlButtonsControls.removeClass("disabled");
+      } else {
+        $controlButtonsControls.addClass("disabled");
+      }
+    });
   });
 })(jQuery);
