bb16200805b4a65c6a1b4450431977bddd7f20d5
Jindrich Mynarz <mynarzjindrich@gmail.com>
2016-07-24 22:36:42 +0200

    Extract SPARQL language constructs during store initialization

--- src/clj/sparqlab/store.clj
+++ src/clj/sparqlab/store.clj
@@ -1,8 +1,26 @@
 (ns sparqlab.store
   (:require [sparqlab.sparql :as sparql]
             [mount.core :as mount]
-            [clojure.tools.logging :as log])
-  (:import [org.apache.jena.rdf.model ModelFactory]))
+            [clojure.tools.logging :as log]
+            [clojure.java.io :as io])
+  (:import [org.apache.jena.rdf.model ModelFactory]
+           [org.apache.jena.query QueryFactory]))
+
+; ----- Data pre-processing -----
+
+(defn enrich-exercises-with-extracted-constructs
+  [store]
+  (let [exercise-queries (->> "sparql/get_exercise_queries.rq"
+                              io/resource
+                              slurp
+                              (sparql/select-query store))
+        extract-constructs (fn [{{exercise "@id"} :exercise
+                                 {query "@value"} :query}]
+                             (map #(assoc {:exercise exercise} :construct %)
+                                  (sparql/extract-language-constructs (QueryFactory/create query))))
+        update-operation (sparql/sparql-template "enrich_exercises_with_extracted_constructs"
+                                                 {:constructs (mapcat extract-constructs exercise-queries)})]
+    (sparql/update-operation store update-operation)))
 
 ; ----- Component -----
 
@@ -10,7 +28,7 @@
   []
   (let [store (ModelFactory/createDefaultModel)]
     (.read store "exercises.ttl") ; Fixture with exercises
-    store))
+    (enrich-exercises-with-extracted-constructs store)))
 
 (defn- close-store 
   [store]
