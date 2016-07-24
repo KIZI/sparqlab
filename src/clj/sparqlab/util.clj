e3fe150200c88603086dab6deefed296e2525d46
Jindrich Mynarz <mynarzjindrich@gmail.com>
2016-07-24 21:27:01 +0200

    Split file predicates

--- src/clj/sparqlab/util.clj
+++ src/clj/sparqlab/util.clj
@@ -1,6 +1,14 @@
 (ns sparqlab.util
   (require [clojure.string :as string]))
 
+(defn is-file?
+  [file]
+  (.isFile file))
+
+(defn has-suffix?
+  [suffix file]
+  (string/ends-with? (.getName file) suffix))
+
 (def query-file?
   "Predicate testing if file is named *.rq."
-  (every-pred #(.isFile %) (comp #(string/ends-with? % ".rq") #(.getName %))))
+  (every-pred is-file? (partial has-suffix? ".rq")))
