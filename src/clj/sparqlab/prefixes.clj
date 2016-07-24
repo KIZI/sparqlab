17e895ed4974cd314f7c4854f9fd94a10c45da4f
Jindrich Mynarz <mynarzjindrich@gmail.com>
2016-07-24 20:53:54 +0200

    UUID IRI helper

--- src/clj/sparqlab/prefixes.clj
+++ src/clj/sparqlab/prefixes.clj
@@ -10,8 +10,16 @@
 (def exercise
   (prefix "http://mynarz.net/sparqlab/resource/exercise/"))
 
+(def local
+  (prefix "http://localhost/"))
+
 (def rdfs
   (prefix "http://www.w3.org/2000/01/rdf-schema#"))
 
 (def xsd
   (prefix "http://www.w3.org/2001/XMLSchema#"))
+
+(defn uuid-iri
+  "Generate a UUID-based localhost IRI."
+  []
+  (local (str (java.util.UUID/randomUUID))))
