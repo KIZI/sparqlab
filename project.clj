849edc785db36d4150cb9ef7682a2e3a3cf99187
Jindrich Mynarz <mynarzjindrich@gmail.com>
2016-08-05 15:14:51 +0200

    Add JSON-LD dependency

--- project.clj
+++ project.clj
@@ -26,7 +26,8 @@
                  [org.apache.jena/jena-arq "3.1.0"]
                  [cheshire "5.6.3"]
                  [stencil "0.5.0"]
-                 [org.topbraid/spin "2.0.0"]]
+                 [org.topbraid/spin "2.0.0"]
+                 [com.github.jsonld-java/jsonld-java "0.8.3"]]
 
   :repositories [["org.topbraid" "http://topquadrant.com/repository/spin"]]
   :min-lein-version "2.0.0"
