2f9de9e3faa30a2c7f9c24a7eeb255e6d46c2ff0
Jindrich Mynarz <mynarzjindrich@gmail.com>
2016-09-09 16:12:47 +0200

    Prefix SPARQL endpoint's URL by servlet context

--- resources/public/js/editor.js
+++ resources/public/js/editor.js
@@ -18,7 +18,7 @@ var escapeHTML = function (text) {
     var yasqe = YASQE.fromTextArea(document.getElementById("editor"), {
       autofocus: true,
       sparql: {
-        endpoint: "../api/query",
+        endpoint: context + "/api/query",
         requestMethod: "GET"
       },
       tabSize: 2,
