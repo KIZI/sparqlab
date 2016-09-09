58b563462e1582eadfb3416edad6054b500db243
Jindrich Mynarz <mynarzjindrich@gmail.com>
2016-09-09 16:05:22 +0200

    Rough start script

---
+++ bin/start.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# 
+# Starts and connects the required Docker images
+# TODO: Migrate this to Docker Compose.
+
+set -e
+
+docker network create sparqlab
+
+###############
+### Stardog ###
+###############
+
+# cd stardog directory...
+docker build -t stardog .
+docker run --name stardog \
+           --net sparqlab \
+           -d stardog
+
+################
+### SPARQLab ###
+################
+
+# cd sparqlab directory...
+docker build -t sparqlab .
+docker run --name sparqlab \
+           -p 3000:3000 \
+           --net sparqlab \
+           -d \
+           -e SPARQL_ENDPOINT=http://stardog:5820/sparqlab/query \
+           -e APP_CONTEXT=/sparqlab \
+           sparqlab
+
+# Open shell in a running image...
+# docker exec -it stardog bash
