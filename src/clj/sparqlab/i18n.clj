299ff4064f76c5ef9fcb7af25c562b3cb753b2c3
Jindrich Mynarz <mynarzjindrich@gmail.com>
2017-06-04 21:00:54 +0200

    English locale

--- src/clj/sparqlab/i18n.clj
+++ src/clj/sparqlab/i18n.clj
@@ -55,4 +55,57 @@
                                 :search-query-constructs "Hledané SPARQL konstrukty"
                                 :title "Nalezená cvičení"}
                :sparql-syntax-error {:title "Chyba syntaxe dotazu"
-                                     :expected-label "Očekáváno"}}}})
+                                     :expected-label "Očekáváno"}}
+          :en {:missing "Missing English text"
+               :about {:title "About"}
+               :and "and"
+               :base {:beta-version "Beta version"
+                      :by-category "By categories"
+                      :by-difficulty "By difficulty"
+                      :by-language-constructs "By language constructs"
+                      :data-title "Data"
+                      :exercise "Exercises"
+                      :search "Search"
+                      :search-query "Search term"
+                      :sparql-endpoint "SPARQL endpoint"}
+               :close "Close"
+               :data {:title "Data"}
+               :endpoint {:title "SPARQL endpoint"
+                          :run-query "Run the query"}
+               :error {:title "Error"}
+               :error-modal {:label "Error in query execution"
+                             :message "There was an error in query execution."}
+               :evaluation {:confirm-reveal {:label "Confirm revealing of the solution"
+                                             :no "No"
+                                             :question "Are you sure you want to reveal the solution?"
+                                             :yes "Yes"}
+                            :correct-answer "Right answer"
+                            :incorrect-answer "Wrong answer"
+                            :missing-constructs "Missing constructs"
+                            :prohibited-constructs "Prohibited constructs"
+                            :results "Results"
+                            :reveal-solution "Reveal solution"
+                            :solution "Solution"
+                            :title "Exercise evaluation: %1"}
+               :exercises {:difficulty "difficulty"
+                           :note-title "Note"
+                           :prohibited-constructs "Prohibited constructs"
+                           :required-constructs "Required constructs"
+                           :revealed "Revealed"
+                           :similar-exercises "Related exercises"
+                           :solved "Solved"
+                           :submit "Submit"}
+               :exercises-by-category {:title "Exercises by category"}
+               :exercises-by-difficulty {:title "Exercises by difficulty"}
+               :exercises-by-language-constructs {:note "The exercises are sorted by the number of language constructs they require, starting with those that require the least constructs."
+                                                  :title "Exercises by language constructs"}
+               :internal-error {:message "Internal application error"
+                                :title "Application error"}
+               :loading "Loading"
+               :not-found {:title "Page not found"}
+               :search-results {:not-found "No exercises were found."
+                                :search-query "Search term"
+                                :search-query-constructs "Searched SPARQL constructs"
+                                :title "Exercises found"}
+               :sparql-syntax-error {:title "Invalid query syntax"
+                                     :expected-label "Expected"}}}})
